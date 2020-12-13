9d5a4cd763798835bdc8c1f41b64e08863e0e90e
santt3131 <santt31@gmail.com>
2020-12-13 15:26:26 +0100

    Only svgo- disable cleanupIDs

--- imagemin.config.js
+++ imagemin.config.js
@@ -1,12 +1,8 @@
 module.exports = {
-    "gifsicle": { "optimizationLevel": 2, "interlaced": false, "colors": 10 },
-    "mozjpeg": { "progressive": true, "quality": 10 },
-    "pngquant": { "quality": [0.25, 0.5] },
     "svgo": {
       "plugins": [
         { "removeViewBox": false },
         { "cleanupIDs": false },
       ]
-    },
-    "webp": { "quality": 10 }
+    }
   }
